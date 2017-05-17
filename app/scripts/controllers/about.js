3c0286cb510d686d8e1e2c832ed16389532229ae
Shivang Bhandari <pump382156@gmail.com>
2017-05-17 21:08:03 +0530

    init commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name venturesityApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the venturesityApp
+ */
+angular.module('venturesityApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name venturesityApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the venturesityApp
+ */
+angular.module('venturesityApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
