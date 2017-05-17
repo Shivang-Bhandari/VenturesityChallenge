291f687df4d06b3eae612c7f145c168c92528869
Shivang Bhandari <pump382156@gmail.com>
2017-05-17 21:33:44 +0530

    init commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,29 +9,4 @@
  * Main module of the application.
  */
 angular
-  .module('venturesityApp', [
-    'ngAnimate',
-    'ngAria',
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+  .module('venturesityApp', []);
