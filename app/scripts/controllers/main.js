5557514375ac3ef4239b7934bedada97560a50ab
Shivang Bhandari <pump382156@gmail.com>
2017-05-19 03:35:55 +0530

    UI Updates

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,4 @@
 'use strict';
-
-
 /**
  * @ngdoc function
  * @name venturesityApp.controller:MainCtrl
@@ -10,12 +8,33 @@
  */
 angular.module('venturesityApp')
   .controller('getRequest', function ($scope, $http) {
-    $scope.formFields='';
+
+    // variable declarations
+    $scope.formFields=''; //hold json to parse
+    $scope.dataObj={}; //holds data to post
+
+    // config setup for form post
+    var config = {
+      headers : {
+        'Content-Type': 'application/json'
+      }
+    };
+
+
+    // function to get json
     $scope.jsonCall = function() {
       $http.get('http://randomform.herokuapp.com')
         .then(function(response) {
-          $scope.formFields = response.data.data.form_fields;
-          console.log(response.data.data.form_fields);
+          $scope.formFields = response.data.data.form_fields; //sets variable eq to json to parse
+          console.log(response.data.data.form_fields); //just a check for proper json incoming
+        });
+    };
+
+    // function to post json
+    $scope.dataPost = function(){
+      $http.post('https://randomform.herokuapp.com/submit', dataObj ,config)
+        .then(function() {
+
         });
     };
 
